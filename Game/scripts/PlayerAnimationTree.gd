ba3849d078d80dc7f462b6c922f48b086a9e7cad
Han Wu <wuhanstudios@gmail.com>
2026-08-24 23:14:45 +0100

    [vfx] Add landing particle VFX

---
+++ Game/scripts/PlayerAnimationTree.gd
@@ -0,0 +1,27 @@
+extends AnimationTree
+
+enum {
+	Normal,
+	Airbone,
+	Dead
+}
+
+var state
+
+func changeStatetoAirbone(): 
+	state = Airbone
+
+func changeStatetoNormal():
+	state = Normal
+
+func checkIsAirbone():
+	return state == Airbone
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- Game/scripts/player.gd
+++ Game/scripts/player.gd
@@ -5,9 +5,17 @@ const SPEED = 10.0
 const JUMP_VELOCITY = 8
 
 @onready var animation_tree: AnimationTree = $LittleAdventurerAndie/AnimationTree
+@onready var footstep_vfx: GPUParticles3D = $VFX/Footstep_VFX
 
 func _process(delta):
+	handleMovementVFX()
+
 	animation_tree.set("parameters/StateMachine/GroundMovement/blend_position", abs(velocity.x))
+	animation_tree.set("parameters/StateMachne/Airbone/blend_position", velocity.y)
+	if is_on_floor():
+		animation_tree.changeStatetoNormal()
+	else:
+		animation_tree.changeStatetoAirbone()
 
 func _physics_process(delta: float) -> void:
 	if velocity.x != 0:
@@ -25,6 +33,7 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		playerGroundSmokeVFX()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -35,3 +44,27 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+func handleMovementVFX():
+	if is_on_floor():
+		if velocity.x != 0:
+			footstep_vfx.emitting = true
+		else:
+			footstep_vfx.emitting = false
+	else:
+		footstep_vfx.emitting = false
+	
+	if is_on_floor():
+		if animation_tree.checkIsAirbone():
+			playerGroundSmokeVFX()
+
+func playerGroundSmokeVFX():
+	var vfxToSpawn = preload("res://Asset/VFX/Scene/land_vfx.tscn")
+	var vfxInstance = vfxToSpawn.instantiate()
+	get_tree().get_root().get_node("Root").add_child(vfxInstance)
+	vfxInstance.global_position = global_position + Vector3(0, 0.3, 0.2)
+	vfxInstance.restart()
+	
+	await get_tree().create_timer(0.6).timeout
+	
+	vfxInstance.queue_free()
